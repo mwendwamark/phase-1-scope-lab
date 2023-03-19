d365caa364d5e983b08b63bb2984f42e2cf6d871
mark mwendwa <mwendwamark310@gmail.com>
2023-03-19 12:43:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase();
+
+    return customerName;
+}
+
+function setBestCustomer() {
+
+    window.bestCustomer;
+
+    bestCustomer = "not bob";
+
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = "maybe bob";
+
+    return bestCustomer;
+    
+}
+
+function changeLeastFavoriteCustomer() {
+    throw "Assignment to constant variable."
+}
